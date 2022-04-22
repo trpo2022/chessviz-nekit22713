276bbb3477e7253c5523a6ce4d763903ad3d9f11
nekit22713 <98864976+nekit22713@users.noreply.github.com>
2022-04-22 21:36:52 +0700

    Update move.h

--- src/lib/move.h
+++ src/lib/move.h
@@ -1,1 +1,11 @@
+#pragma once
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+void yfcheck(int* yf);
+void yscheck(int* ys);
+void printerror();
+void move(char desk[9][9], int xs, int xf, int ys, int yf);
+int movecheck(char desk[9][9], int color);
+void readmove(char desk[9][9], int finish, int turnp);
