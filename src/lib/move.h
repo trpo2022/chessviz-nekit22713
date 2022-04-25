0e5a5f08824ac6343444001b3f5ede2726a99a13
nekit22713 <98864976+nekit22713@users.noreply.github.com>
2022-04-25 13:30:17 +0700

    Update move.h

--- src/lib/move.h
+++ src/lib/move.h
@@ -1,11 +1,10 @@
 #pragma once
 #include <math.h>
+#include <stdbool.h> 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdbool.h> 
 #define desk_size 8
-
 
 typedef struct motion {
     int y1;
@@ -19,46 +18,27 @@ void printdesk(char desk[desk_size][desk_size]);
 void translate(char* step, motion* motion);
 
 bool moveKing(
-        char desk[desk_size][desk_size],
-        motion motion,
-        bool error_output);
+        char desk[desk_size][desk_size], motion motion, bool error_output);
 
 bool moveQueen(
-        char desk[desk_size][desk_size],
-        motion motion,
-        bool error_output);
+        char desk[desk_size][desk_size], motion motion, bool error_output);
 
 bool moveRook(
-        char desk[desk_size][desk_size],
-        motion motion,
-        bool error_output);
+        char desk[desk_size][desk_size], motion motion, bool error_output);
 
 bool moveKnight(
-        char desk[desk_size][desk_size],
-        motion motion,
-        bool error_output);
+        char desk[desk_size][desk_size], motion motion, bool error_output);
 
 bool moveBishop(
-        char desk[desk_size][desk_size],
-        motion motion,
-        bool error_output);
+        char desk[desk_size][desk_size], motion motion, bool error_output);
 
 bool movePawn(
-        char* step,
-        motion motion,
-        char desk[desk_size][desk_size],
-        bool moveW,
+        char* step, motion motion, char desk[desk_size][desk_size], bool moveW,
         bool error_output);
 
 bool movecheck(
-        char* step,
-        motion motion,
-        char desk[desk_size][desk_size],
-        bool moveW,
+        char* step, motion motion, char desk[desk_size][desk_size], bool moveW,
         bool output);
 
 int turn(
-        char* step,
-        motion motion,
-        char desk[desk_size][desk_size],
-        bool moveW);
+        char* step, motion motion, char desk[desk_size][desk_size], bool moveW);
